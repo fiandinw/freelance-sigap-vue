c93af682bda2c337f7ad874ce88dcc2f0388bdaf
fiandinw <alifiandinursanni@gmail.com>
2022-07-30 17:48:19 +0700

    fb

--- src/components/AuthExternalGroup.vue
+++ src/components/AuthExternalGroup.vue
@@ -2,7 +2,12 @@
   import iconFacebook from "../assets/icons/facebook.png";
   import iconGoogle from "../assets/icons/google.png";
   import LoginWith from "./LoginWith.vue";
-  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+  } from "firebase/auth";
   import { useRouter } from "vue-router";
   import swal from "sweetalert";
 
@@ -10,6 +15,7 @@
   const auth = getAuth();
 
   const provider = new GoogleAuthProvider();
+  const provider2 = new FacebookAuthProvider();
 
   const handleGoogle = () => {
     signInWithPopup(auth, provider)
@@ -37,10 +43,41 @@
         swal(error.code, error.message, "error");
       });
   };
+
+  const handleFacebook = () => {
+    signInWithPopup(auth, provider2)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        swal("Login Berhasil", "Selamat Datang", "success").then(() => {
+          router.push({ name: "home" });
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+        // ...
+        console.log(error.code, error.message);
+        swal(error.code, error.message, "error");
+      });
+  };
 </script>
 
 <template>
-  <LoginWith :icon="iconFacebook" title="Lanjutkan dengan Facebook" />
+  <LoginWith
+    @click="handleFacebook"
+    :icon="iconFacebook"
+    title="Lanjutkan dengan Facebook"
+  />
   <LoginWith
     @click="handleGoogle"
     :icon="iconGoogle"
